cf2e711074485aac8c361927f6c4687e6950406d
Tanvi11-d <Tanvi.d@bitcodingsolutions.com>
2026-04-20 17:59:42 +0530

    feat:files added

--- multi_agent.py
+++ multi_agent.py
@@ -11,21 +11,23 @@ from langsmith import traceable
 
 # load env 
 load_dotenv() 
+
 GIT_USER_ID = os.getenv("git_user_id") 
 USER_ID=os.getenv("user_id") 
 api_key=os.getenv("GROQ_API_KEY") 
 
 model = ChatGroq( 
-    model="openai/gpt-oss-120b", 
+    model="moonshotai/kimi-k2-instruct-0905", 
     api_key=api_key 
 ) 
 
 # web search agent 
 search=DuckDuckGoSearchRun() 
 
+
 @tool 
 def web_search(query:str): 
-    """Search the web for information.Useful for searching information on the internet.Use this when you need to find current or factual information.""" 
+    """Search the web for information.Useful for searching latest information on the internet.Use this when you need to find current or factual information.""" 
     result=search.invoke(query) 
     return result 
 
@@ -36,8 +38,9 @@ create_web_agent=create_agent(
 @tool
 def web_agent_tool(query: str):
     """Use for web search queries."""
-    res = create_web_agent.invoke({"messages": [{"role": "user", "content": query}]})
-    return res["messages"][-1].content
+    response = create_web_agent.invoke({"messages": [{"role": "user", "content": query}]})
+    return response.get('messages')[-1].content
+
 
 # genearate jokes agent 
 @tool 
@@ -66,7 +69,8 @@ create_joke_agent=create_agent(
 def joke_agent_tool(query: str):
     """Use for generating jokes."""
     res = create_joke_agent.invoke({"messages": [{"role": "user", "content": query}]})
-    return res["messages"][-1].content
+    return res.get('messages')[-1].content
+
 
 # write poem agent
 @tool
@@ -93,20 +97,20 @@ create_poem_agent=create_agent(
 @tool
 def poem_agent_tool(query: str):
     """Use for writing poems."""
-    res = create_poem_agent.invoke({
-        "messages": [{"role": "user", "content": query}]
-    })
-    return res["messages"][-1].content
+    poem_result = create_poem_agent.invoke({"messages": [{"role": "user", "content": query}]})
+    return poem_result.get('messages')[-1].content
 
-composio = Composio( provider=LangchainProvider() ) 
+
+composio = Composio( 
+    provider=LangchainProvider() 
+) 
 
 # github agent 
 git_tools = composio.tools.get( 
     user_id=GIT_USER_ID, 
     tools=[
-        "GITHUB_CREATE_A_REPOSITORY_PROJECT", "GITHUB_GET_THE_WEEKLY_COMMIT_COUNT","GITHUB_DELETE_A_FILE" , 
-        "GITHUB_UPDATE_A_PROJECT","GITHUB_GET_A_REPOSITORY","GITHUB_GET_REPOSITORY_OWNER", 
-        "GITHUB_LIST_COMMITS","GITHUB_RENAME_A_BRANCH","GITHUB_COMMIT_MULTIPLE_FILES" ] ) 
+        "GITHUB_CREATE_A_REPOSITORY_PROJECT", "GITHUB_GET_THE_WEEKLY_COMMIT_COUNT","GITHUB_DELETE_A_FILE", 
+        "GITHUB_GET_A_REPOSITORY","GITHUB_RENAME_A_BRANCH","GITHUB_COMMIT_MULTIPLE_FILES","GITHUB_GET_A_BRANCH"]) 
 
 create_git_agent = create_agent( 
     model=model, 
@@ -120,45 +124,51 @@ create_git_agent = create_agent(
     - Always follow the tool schema. 
     - Fill required fields properly. 
     - Keep responses accurate.
-    - Perform GitHub actions correctly
-    - Always confirm final action clearly
-    - Do not return raw JSON """ ) 
+    - Perform GitHub actions correctly.
+    - Always confirm final action clearly.
+    - Do not return raw JSON. 
+""" 
+) 
+
 
 @tool
 def github_agent_tool(query: str):
     """Use for GitHub operations."""
     res = create_git_agent.invoke({"messages": [{"role": "user", "content": query}]})
-    return res["messages"][-1].content
+    return res.get('messages')[-1].content
 
 # gmail agent 
 gmail_tools = composio.tools.get( 
     user_id=USER_ID,
-    tools=["GMAIL_FETCH_EMAILS", "GMAIL_SEND_EMAIL", "GMAIL_FORWARD_MESSAGE", "GMAIL_GET_DRAFT", "GMAIL_SEND_DRAFT" ] ) 
+    tools=["GMAIL_FETCH_EMAILS","GMAIL_SEND_EMAIL","GMAIL_FORWARD_MESSAGE","GMAIL_CREATE_EMAIL_DRAFT","GMAIL_LIST_DRAFTS"] ) 
+
 
 create_gmail_agent = create_agent( 
     model=model, 
     tools=gmail_tools, 
     system_prompt = """ When calling any tool: 
     - Keep responses concise and accurate. 
-    - If field type is : - string -> use " " (empty string, NEVER null). 
-    - array -> use []. - boolean -> use True/False. 
+    - If field type is : 
+    - string -> use " " (empty string, NEVER null). 
+    - array -> use []. 
+    - boolean -> use True/False. 
     - integer -> use 0. 
     - Fill required fields properly. 
     - If user say fetch messages then your are show body or subject and from. 
-    - Always format professionally
-    - When sending an email, always use the provided sender_name in the "Best regards" section; if not provided, use the sender email name, and never generate random names.
-
-""" ) 
+    - Always format professionally.
+    - When sending an email, always use the provided sender name in the "Best Regards" section; if not provided, use the sender name, and never guess the sender names.
+""" 
+) 
 
 @tool
 def gmail_agent_tool(query: str):
     """Use for Gmail operations."""
     res = create_gmail_agent.invoke({"messages": [{"role": "user", "content": query}]})
-    return res["messages"][-1].content
+    return res.get('messages')[-1].content
+
 
 system_prompt="""
-You are an intelligent multi-agent AI system.
-
+-You are an intelligent multi-agent AI system.
 You have:
 - joke agent
 - poem agent
@@ -166,30 +176,27 @@ You have:
 - github agent
 - web search agent
 
-IMPORTANT RULES:
+Important Rules:
 
 1. Understand user intent clearly.
 2. If task requires multiple steps:
    - First call required agent
    - Then use output in next agent
 -DO NOT skip steps.
+
 Example:
 - send joke to email
   Step 1: generate joke
   Step 2: send email with joke
 
-3. Always return final response.
-4. Never return raw tool output.
-5. Response rules:
-- Joke → return joke
-- Poem → return poem
-- Email → confirm action
-- GitHub → confirm action
-6. Do not mention tools.
-7. Be smart and chain agents when required.
+3. Response rules:
+- Joke -> return joke
+- Poem -> return poem
+- Email -> confirm action
+- GitHub -> confirm action
+4. Do not mention which tool you used.
+5. Be smart and chain agents when required.
 """
-
-
 
 main_agent = create_agent(
     model=model,
@@ -200,17 +207,16 @@ main_agent = create_agent(
         github_agent_tool,
         gmail_agent_tool
     ],
-
     system_prompt=system_prompt
 )
-     
+    
 @traceable(name="call_agent")
 def call_agent(query: str):
     try:
-        logger.info("Running main agent...")
-        response = main_agent.invoke({"messages": [{"role": "user", "content": query}]})
+        logger.info("running main agent...")
+        result = main_agent.invoke({"messages":[{"role": "user", "content": query}]})
         logger.info("Response generated")
-        return response["messages"][-1].content
+        return result.get('messages')[-1].content
     except Exception as e:
         logger.error("Agent error")
-        raise HTTPException(status_code=500, detail=str(e))
+        raise HTTPException(status_code=500, detail=f"error in call-agent{e}")
